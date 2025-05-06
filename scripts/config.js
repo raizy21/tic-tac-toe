dce38b1b618729997c3cc17aeae9f35c8b485133
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-06 12:12:10 +0200

    [code] making the valiadtion and the error for player name

--- scripts/config.js
+++ scripts/config.js
@@ -12,4 +12,23 @@ function closePlayerConfig() {
 
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error"); // remove the error class from the first element of the form
+  errorOutputElement.textContent = ""; // clear the error message
+}
+
+function savePlayerConfig(event) {
+  console.log("savePlayerConfig function called");
+
+  event.preventDefault(); // prevent the form from submitting and reloading the page
+  console.log(event);
+
+  const formData = new FormData(event.target); // get the form data from the event target
+  const enteredPlayerName = formData.get("player-name").trim(); // get the player name from the form data and trim it
+  console.log(enteredPlayerName);
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error"); // add the error class to the first element of the form
+    errorOutputElement.textContent = "please enter a valid name!"; // if the player name is empty, show an error message
+    return;
+  }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 
+const formElement = document.querySelector("form");
+const errorOutputElement = document.getElementById("config-errors");
+
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 
@@ -15,3 +18,5 @@ editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
