782f9917b65115bc2287169b7c2b0cd3aa8fed92
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-06 11:36:01 +0200

    [code] adding the close overlay funtion

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,15 @@
 // this file contains the configuration for the player
 function openPlayerConfig() {
   console.log("openPlayerConfig function called");
+
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+// this function closes the player configuration overlay and the backdrop
+function closePlayerConfig() {
+  console.log("closePlayerConfig function called");
+
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 // console.log("checking if script is loaded");
 
 // selecting elements from the DOM
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 
 //event listeners
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
