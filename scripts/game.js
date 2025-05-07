7fc011f8897031bbd67fa7817e7acd343903dabe
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-07 08:27:22 +0200

    [code] adding logik when the players are empty

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,8 @@
 function startNewGame() {
+  if (!players[0].name || !players[1].name) {
+    alert("please set custom player names for both players!");
+    return;
+  }
+
   gameAreaElement.style.display = "block";
 }
