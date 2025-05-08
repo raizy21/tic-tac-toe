34ab4a433621f3f0d79207ce348aae24122adb74
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-08 06:58:48 +0200

    [code] trying to update the player name

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,12 @@
-// dispay the game area
+// display the game area
 function startNewGame() {
   if (!players[0].name || !players[1].name) {
     alert("please set custom player names for both players!");
     return;
   }
 
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  console.log(players[activePlayer].name);
   gameAreaElement.style.display = "block";
 }
 
@@ -15,11 +17,20 @@ function switchPlayer() {
   } else {
     activePlayer = 0;
   }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  // console.log(activePlayer);
 }
 
 // select area on the game board
 function selectGameField(event) {
+  console.log(event.target.tagName);
+
+  if (event.target.tagName !== "LI") {
+    return;
+  }
+
   event.target.textContent = players[activePlayer].symbol;
   event.target.classList.add("disabled");
+
   switchPlayer();
 }
--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,7 @@ const errorOutputElement = document.getElementById("config-errors");
 
 const gameAreaElement = document.getElementById("active-game");
 
+const activePlayerNameElement = document.querySelector("#active-player-name");
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 
